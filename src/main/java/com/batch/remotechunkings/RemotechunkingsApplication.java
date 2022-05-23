fc9bc72b592d1b8d5997c185c4653ca6f5e59faa
hojjatshabab <hojatsba@gmail.com>
2022-05-23 12:51:02 +0430

    Add project

---
+++ src/main/java/com/batch/remotechunkings/RemotechunkingsApplication.java
@@ -0,0 +1,13 @@
+package com.batch.remotechunkings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotechunkingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotechunkingsApplication.class, args);
+	}
+
+}
